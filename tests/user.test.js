3f25e223d7f929ea90b1a146cee6f369f15072aa
snpr <vaugk13@gmail.com>
2019-07-29 15:30:00 +0500

    Add user creation test; Clear test DB before each test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,1 +1,18 @@
 const request = require("supertest");
+const app = require("../src/app");
+const User = require("../src/models/user");
+
+beforeEach(async () => {
+  await User.deleteMany();
+});
+
+test("Should signup a new user", async () => {
+  await request(app)
+    .post("/users")
+    .send({
+      name: "SNPR",
+      email: "awedref@gmail.com",
+      password: "(AY*NSlANBJssdfgl"
+    })
+    .expect(201);
+});
